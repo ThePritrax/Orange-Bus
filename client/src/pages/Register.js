b19c917057a0a6d2ae564e177293b019b0106e08
ThePritrax <pritrax@gmail.com>
2023-02-06 19:39:20 +0530

    register page styling

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {Form} from 'antd';
+import {Link} from 'react-router-dom';
 
 function Register() {
   return (
     <div className='h-screen d-flex justify-content-center align-items-center'>
-        <div className='w-300'>
+        <div className='w-400 card p-3'>
+            <h1 className='text-3xl'>OrangeBus - Register</h1>
+            <hr></hr>
         <Form layout='vertical'>
             <Form.Item label='Name'>
                 <input type="text"/>
@@ -15,6 +18,12 @@ function Register() {
             <Form.Item label='Password'>
                 <input type="text"/>
             </Form.Item>
+            <div className="d-flex justify-content-between align-items-center my-3">
+            <Link to="/login">Click Here To Login</Link>
+            <button className="secondary-btn" type="submit">
+              Register
+            </button>
+          </div>
         </Form>
         </div>
     </div>
