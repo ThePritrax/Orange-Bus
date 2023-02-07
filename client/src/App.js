aaedbec50bc5741e2e118858e81991038fe716ac
ThePritrax <pritrax@gmail.com>
2023-02-07 22:54:52 +0530

    setup user model and registeration

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import './resources/global.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import "./resources/global.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,38 +1,37 @@
-import React from 'react'
-import {Form} from 'antd';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Form } from "antd";
+import { Link } from "react-router-dom";
 
 function Register() {
-
-    const onFinish = async (values) => {
-        console.log(values)
-    }
+  const onFinish = async (values) => {
+    console.log(values);
+  };
 
   return (
-    <div className='h-screen d-flex justify-content-center align-items-center'>
-        <div className='w-400 card p-3'>
-            <h1 className='text-3xl'>OrangeBus - Register</h1>
-            <hr></hr>
-        <Form layout='vertical' onFinish={onFinish}>
-            <Form.Item label='Name' name='Name'>
-                <input type="text"/>
-            </Form.Item>
-            <Form.Item label='Email' name='Email'>
-                <input type="text"/>
-            </Form.Item>
-            <Form.Item label='Password' name='Password'>
-                <input type="password"/>
-            </Form.Item>
-            <div className="d-flex justify-content-between align-items-centerS">
+    <div className="h-screen d-flex justify-content-center align-items-center">
+      <div className="w-400 card p-3">
+        <h1 className="text-3xl">OrangeBus - Register</h1>
+        <hr></hr>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item label="Name" name="Name">
+            <input type="text" />
+          </Form.Item>
+          <Form.Item label="Email" name="Email">
+            <input type="text" />
+          </Form.Item>
+          <Form.Item label="Password" name="Password">
+            <input type="password" />
+          </Form.Item>
+          <div className="d-flex justify-content-between align-items-centerS">
             <Link to="/login">Click Here To Login</Link>
             <button className="secondary-btn" type="submit">
               Register
             </button>
           </div>
         </Form>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
