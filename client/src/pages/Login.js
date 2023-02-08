b9c10ede08b5ea90d4bdc13b65968f8ef735ff3a
ThePritrax <pritrax@gmail.com>
2023-02-09 05:20:18 +0530

    login and register endpoints

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import {Form} from 'antd';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Form, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
-
-    const onFinish = async (values) => {
-        console.log(values)
+  const navigate = useNavigate()
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post("/api/users/login", values);
+      if (response.data.success) {
+        message.success(response.data.message);
+        localStorage.setItem("token", response.data.data);
+        navigate("/");
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      message.error(error.message);
     }
+  };
 
   return (
     <div className='h-screen d-flex justify-content-center align-items-center'>
@@ -14,10 +26,10 @@ function Login() {
             <h1 className='text-3xl'>OrangeBus - Login</h1>
             <hr></hr>
         <Form layout='vertical' onFinish={onFinish}>
-            <Form.Item label='Email' name='Email'>
+            <Form.Item label='Email' name='email'>
                 <input type="text"/>
             </Form.Item>
-            <Form.Item label='Password' name='Password'>
+            <Form.Item label='Password' name='password'>
                 <input type="password"/>
             </Form.Item>
             <div className="d-flex justify-content-between align-items-centerS">
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/usersModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 /* register new user */
 router.post("/register", async (req, res) => {
@@ -31,4 +32,47 @@ router.post("/register", async (req, res) => {
   }
 });
 
-module.exports = router
+/* user login */
+
+router.post("/login", async (req, res) => {
+  try {
+    const userExists = await User.findOne({ email: req.body.email });
+    if (!userExists) {
+      return res.send({
+        message: "User does not exist",
+        success: false,
+        data: null,
+      });
+    }
+
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      userExists.password
+    );
+    if (!passwordMatch) {
+      return res.send({
+        message: "Incorrect password",
+        success: false,
+        data: null,
+      });
+    }
+
+    const token = jwt.sign({ userId: userExists._id }, process.env.jwt_secret, {
+      expiresIn: "1d",
+    });
+    res.send({
+      message: "User logged in successfully",
+      success: true,
+      data: token,
+    });
+
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+      data: null,
+    });
+  }
+});
+
+module.exports = router;
