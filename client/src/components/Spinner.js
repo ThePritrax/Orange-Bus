140b5e4916fa62ffa16609d915b0108d66016d67
ThePritrax <pritrax@gmail.com>
2023-02-09 19:27:04 +0530

    loading spinner styled

---
+++ client/src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Spinner() {
+  return (
+    <div className="spinner-parent">
+      <div class="spinner-border" role="status">
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
