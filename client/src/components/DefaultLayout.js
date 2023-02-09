793ede18645e7d43660fdfca38b5bb795eb4374a
ThePritrax <pritrax@gmail.com>
2023-02-10 01:38:21 +0530

    layout structure and sidebar styling

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -1,12 +1,66 @@
-import React from 'react'
+import React from "react";
+import '../resources/layout.css';
+import { useNavigate } from "react-router-dom";
+
 
 function DefaultLayout({children}) {
+  const navigate = useNavigate();
+  const userMenu = [];
+  const adminMenu = [
+    {
+      name: 'Home',
+      path: '/admin',
+      icon: 'ri-home-2-line',
+    },
+    {
+      name: 'Buses',
+      path: '/admin/buses',
+      icon: 'ri-bus-line',
+    },
+    {
+      name: 'Users',
+      path: '/admin/users',
+      icon: 'ri-user-line',
+    },
+    {
+      name: 'Bookings',
+      path: '/admin/bookings',
+      icon: 'ri-file-list-line',
+    },
+    {
+      name: 'Logout',
+      path: 'logout',
+      icon: 'ri-logout-box-line',
+    }
+  ];
+  const menuToBeRendered = adminMenu
+  
   return (
-    <div>
-        <h1>Header</h1>
-        {children}
+    <div className='layout-parent'>
+        <div className='sidebar'>
+          <div className="d-flex flex-column gap-2 justify-content-start">
+          {menuToBeRendered.map((item, index) => {
+            return (
+              <div className="menu-item">
+              <i className={item.icon}></i>
+              <span 
+                onClick={() => {
+                  navigate(item.path);
+              }}>{item.name}
+              </span>
+            </div>
+            );
+          })}
+          </div>
+        </div>
+        <div className='body'>
+          <div className='header'>
+            header
+          </div>
+          <div className='content'>{children}</div>
+        </div>
     </div>
-  )
+  );
 }
 
 export default DefaultLayout
