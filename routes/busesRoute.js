156b0f28eec6fc94f20565329a0d6796843f29c4
ThePritrax <pritrax@gmail.com>
2023-02-12 14:01:25 +0530

    get bus stable point 1

--- routes/busesRoute.js
+++ routes/busesRoute.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const Bus = require("../models/busModel");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 // add-bus
-router.post("/add-bus", async (req, res) => {
+router.post("/add-bus", authMiddleware, async (req, res) => {
   try {
     const existingBus = await Bus.findOne({ number: req.body.number });
     if (existingBus) {
@@ -21,5 +22,19 @@ router.post("/add-bus", async (req, res) => {
     res.status(500).send({ success: false, message: error.message });
   }
 });
+
+// get-all-buses
+router.post("/get-all-buses", authMiddleware, async (req, res) => {
+    try {
+      const buses = await Bus.find(req.body);
+      return res.status(200).send({
+        success: true,
+        message: "Buses fetched successfully",
+        data: buses,
+      });
+    } catch (error) {
+      res.status(500).send({ success: false, message: error.message });
+    }
+  });
 
 module.exports = router;
