8e2565633114cf67a7cf23f8e9da5e74c7ca3c51
ThePritrax <pritrax@gmail.com>
2023-02-12 14:05:16 +0530

    get bus stable point 2

--- client/src/pages/Admin/AdminBuses.js
+++ client/src/pages/Admin/AdminBuses.js
@@ -1,22 +1,38 @@
-import React from 'react'
-import PageTitle from '../../components/PageTitle'
-import BusForm from '../../components/BusForm'
+import React,  { useEffect } from "react";
+import PageTitle from "../../components/PageTitle";
+import BusForm from "../../components/BusForm";
+import { useDispatch } from "react-redux";
 
 function AdminBuses() {
-  const [showBusForm, setShowBusForm] = React.useState(false)
+  const dispatch = useDispatch();
+  const [showBusForm, setShowBusForm] = React.useState(false);
+  const [buses, setBuses] = React.useState([]);
+
+  const getBuses = async() => {
+  }
+
+  useEffect(() => {
+    getBuses();
+  }, []);
+
   return (
     <div>
       <div className="d-flex justify-content-between">
         <PageTitle title="Buses" />
-        <button className="primary-btn"
-        onClick={() =>setShowBusForm(true)}
-        >Add Bus</button>
+        <button className="primary-btn" onClick={() => setShowBusForm(true)}>
+          Add Bus
+        </button>
       </div>
 
-      {showBusForm && <BusForm showBusForm={showBusForm} setShowBusForm={setShowBusForm} type='add'/>}
-      
+      {showBusForm && (
+        <BusForm
+          showBusForm={showBusForm}
+          setShowBusForm={setShowBusForm}
+          type="add"
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default AdminBuses
+export default AdminBuses;
